4ce3e2a0f526d1f83767b72dbb59d4bf7e9e3df6
Nellyojay <nellyjohnson696@gmail.com>
2025-08-11 15:56:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("paymentForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+    const data = {};
+    formData.forEach((value, key) => data[key] = value);
+
+    fetch("https://script.google.com/macros/s/AKfycbzv7vh59rSS_qcSbA3fJg7LcNuz7aP3___7gdZKOaeDc3ZoqiWj0U_fua41zKhjMqnY/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(() => {
+        document.getElementById("statusMessage").innerText = "✅ Thank you! Your response has been recorded. If you would like to say or ask more, contact us on the contact info below.";
+        alert('✅ Thank you! Your response has been recorded. If you would like to say or ask more, contact us on the contact info below.')
+        this.reset();
+    }).catch(() => {
+        document.getElementById("statusMessage").innerText = "❌ Something went wrong. Please try again.";
+    });
+});
